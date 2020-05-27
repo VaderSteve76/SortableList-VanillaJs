4a081111549a962d803da0e25ca10565a96e1a66
VaderSteve76 <alverson.s@yahoo.com>
2020-05-27 14:35:17 -0400

    Added getElements, info array and event listeners

---
+++ script.js
@@ -0,0 +1,41 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richestPeople = [
+  'Jeff Bezos',
+  'Bill Gates',
+  'Warren Buffett',
+  'Bernard Arnault',
+  'Carlos Slim Helu',
+  'Amancio Ortega',
+  'Larry Ellison',
+  'Mark Zuckerberg',
+  'Michael Bloomberg',
+  'Larry Page'
+];
+
+// Store listitems
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll('.draggable');
+  const dragListItems = document.querySelectorAll('.draggable-list li');
+
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', dragStart);
+  });
+
+  dragListItems.forEach(item => {
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('drop', dragDrop);
+    item.addEventListener('dragenter', dragEnter);
+    item.addEventListener('dragleave', dragLeave);
+  });
+};
+
+check.addEventListener('click', checkOrder);
